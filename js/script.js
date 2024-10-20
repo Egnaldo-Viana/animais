28ed9a4b93251208779da07710374a589e90b62f
egnaldo <egnaldo.viana.2003@gmail.com>
2024-10-20 13:01:39 -0300

    tooltips

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initScrollSuave from "./modules/scroll-suave.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
 
 initAccordion();
 initScrollSuave();
 initTabNav();
 initModal();
+initTooltip();
