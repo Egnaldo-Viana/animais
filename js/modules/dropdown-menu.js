81021adacfcddfcc9f67b8baaae66fdd8b4d918d
egnaldo <egnaldo.viana.2003@gmail.com>
2024-10-20 18:18:59 -0300

    dropdown

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,14 +1,18 @@
-export default function initDropdown() {}
+import outsideClick from "./outsideclick.js";
+export default function initDropdown() {
+  const dropdownMenus = document.querySelectorAll("[data-dropdown");
 
-const dropdownMenus = document.querySelectorAll("[data-dropdown");
-
-dropdownMenus.forEach((menu) => {
-  ["touchstart", "click"].forEach((userEvent) => {
-    menu.addEventListener(userEvent, handleClick);
+  dropdownMenus.forEach((menu) => {
+    ["touchstart", "click"].forEach((userEvent) => {
+      menu.addEventListener(userEvent, handleClick);
+    });
   });
-});
 
-function handleClick(event) {
-  event.preventDefault();
-  this.classList.toggle("active");
+  function handleClick(event) {
+    event.preventDefault();
+    this.classList.toggle("active");
+    outsideClick(this, ["touchstart", "click"], () => {
+      this.classList.remove("active");
+    });
+  }
 }
