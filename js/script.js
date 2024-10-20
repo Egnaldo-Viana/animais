e29e31982304eb991e771b38e3b5fdabb8b4d1f3
egnaldo <egnaldo.viana.2003@gmail.com>
2024-10-20 17:11:59 -0300

    dropdown

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
+import initDropdown from "./modules/dropdown-menu.js";
 
 initAccordion();
 initScrollSuave();
 initTabNav();
 initModal();
 initTooltip();
+initDropdown();
