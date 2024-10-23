eae1b83468fcde824474ff03384e317376e6268b
egnaldo <egnaldo.viana.2003@gmail.com>
2024-10-23 01:42:58 -0300

    animação de números

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdown from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
+import initAnimaNumeros from "./modules/anima-numeros.js";
 
-initAccordion();
 initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
 initTabNav();
 initModal();
 initTooltip();
 initDropdown();
 initMenuMobile();
+initAnimaNumeros();
