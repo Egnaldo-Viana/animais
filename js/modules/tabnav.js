cb9276327eff689ee6c8e8f84ca0234946b65c44
egnaldo <egnaldo.viana.2003@gmail.com>
2024-10-24 21:06:54 -0300

    funcionamento

--- js/modules/tabnav.js
+++ js/modules/tabnav.js
@@ -1,16 +1,16 @@
 export default function initTabNav() {
-  const tabMenu = document.querySelectorAll("[data-tab='menu']  li ");
-  const tabContetnt = document.querySelectorAll('[data-tab="content"] section');
+  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
+  const tabContent = document.querySelectorAll('[data-tab="content"] section');
 
-  if (tabMenu.length && tabContetnt.length) {
-    tabContetnt[0].classList.add("ativo");
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
 
     function activeTab(index) {
-      tabContetnt.forEach((section) => {
+      tabContent.forEach((section) => {
         section.classList.remove("ativo");
       });
-      const direcao = tabContetnt[index].dataset.anime;
-      tabContetnt[index].classList.add("ativo");
+      //const direcao = tabContent[index].dataset.anime;
+      tabContent[index].classList.add("ativo");
     }
 
     tabMenu.forEach((itemMenu, index) => {
